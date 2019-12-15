a4cbe8f8177761d42dfb2c1aedd59dc66507f558
Christoffer Wielsøe <fatherofpiper@Christoffers-iMac.local>
2019-12-15 22:34:40 +0100

    Horizontal scroll added | template filled with Json information

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-getData();
+window.onload = getData();
 function getData() {
     fetch(
         "http://camelsaidwhat.com/musil_wp/wp-json/wp/v2/painting?per_page=100&_embed"
@@ -8,11 +8,41 @@ function getData() {
 }
 
 function showPosts(paintings) {
-    console.log(paintings)
-    paintings.forEach(postPaint)
+    console.log(paintings);
+    paintings.forEach(postPaint);
 }
 
+var counter = 0;
+
+const main = document.querySelector("main");
+const scrollDiv = document.querySelector(".scroll");
+
 function postPaint(paints) {
-    var img = document.querySelector(".painting")
-    img.setAttribute("src", paints.painting.guid)
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+    const title = paints.title.rendered;
+    const des = paints.painting_des;
+    const medium = paints.medium;
+    const painting = paints.painting.guid;
+
+    clone.querySelector(".painting-title").innerHTML = title;
+    clone.querySelector(".painting-des").innerHTML = des;
+    clone.querySelector(".painting").setAttribute("src", painting);
+    clone.querySelector(".box-title").innerHTML = title;
+    clone.querySelector(".medium-text").innerHTML = medium;
+
+    main.appendChild(clone);
+    //HORIZONTAL SCROLL
+    counter++;
+
+    scrollDiv.style.setProperty("--main-width", counter);
 }
+
+
+// HORIZONTAL SCROLL
+window.addEventListener("scroll", function() {
+    var scrollPos = window.scrollY;
+    main.style.left = "-" + scrollPos + "px";
+    console.log(scrollPos);
+
+});
