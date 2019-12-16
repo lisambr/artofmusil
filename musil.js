80d6b7d1e7dcfa6bff6175889710ca7808219b97
Christoffer Skytte Wielsøe <FatherofPiper@Marys-MacBook-Pro.local>
2019-12-16 21:01:02 +0100

    Eventlisteners expanded

--- musil.js
+++ musil.js
@@ -17,31 +17,63 @@ var longBox = document.querySelector(".musil-long");
 var artistTxt = document.querySelector(".the-artist h2");
 var humanTxt = document.querySelector(".the-human h2");
 
-artist.addEventListener("mouseenter", function() {
+artist.addEventListener("mouseenter", hoverArtist);
+artist.addEventListener("mouseleave", leaveArtist);
+artist.addEventListener("click", clickArtist);
+human.addEventListener("mouseenter", hoverHuman);
+human.addEventListener("mouseleave", leaveHuman);
+human.addEventListener("click", clickHuman);
+
+function hoverArtist() {
     shortBox.style.left = "0%";
     artistTxt.style.transform = "translateX(-7%)";
-});
-artist.addEventListener("mouseleave", function() {
+}
+
+function leaveArtist() {
     shortBox.style.left = "5%";
     artistTxt.style.transform = "translateX(0)";
-});
-artist.addEventListener("click", function() {
+    artist.addEventListener("mouseleave", leaveArtist);
+    artist.addEventListener("mouseenter", hoverArtist);
+    window.removeEventListener("click", leaveArtist);
+}
+
+function clickArtist() {
     shortBox.style.left = "-40%";
     artistTxt.style.transform = "translateX(-100%)";
-});
+    artist.removeEventListener("mouseleave", leaveArtist);
+    artist.removeEventListener("mouseenter", hoverArtist);
+    setTimeout(artistClose, 100);
+}
 
-human.addEventListener("mouseenter", function() {
-    longBox.style.right = "0";
+function artistClose() {
+    window.addEventListener("click", leaveArtist);
+}
+
+function hoverHuman() {
+    longBox.style.right = "0%";
     humanTxt.style.transform = "translateX(7%)";
-});
-human.addEventListener("mouseleave", function() {
+}
+
+function leaveHuman() {
     longBox.style.right = "5%";
     humanTxt.style.transform = "translateX(0)";
-});
-human.addEventListener("click", function() {
+    human.addEventListener("mouseleave", leaveHuman);
+    human.addEventListener("mouseenter", hoverHuman);
+    window.removeEventListener("click", leaveHuman);
+}
+
+function clickHuman() {
     longBox.style.right = "-40%";
     humanTxt.style.transform = "translateX(100%)";
-});
+    human.removeEventListener("mouseleave", leaveHuman);
+    human.removeEventListener("mouseenter", hoverHuman);
+    setTimeout(humanClose, 100);
+}
+
+function humanClose() {
+    window.addEventListener("click", leaveHuman);
+}
+
 
 function postDes(description) {
     document.getElementById("musilh3").innerHTML = description.short_des;
